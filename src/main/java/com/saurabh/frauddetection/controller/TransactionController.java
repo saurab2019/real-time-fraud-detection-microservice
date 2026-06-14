d12e3a8722fbf4b2c998454aa5e1b4f5ecf7e0e1
Saurabh Garg <56071783+saurab2019@users.noreply.github.com>
2026-06-14 17:09:39 +0530

    Added Swagger documentation

--- src/main/java/com/saurabh/frauddetection/controller/TransactionController.java
+++ src/main/java/com/saurabh/frauddetection/controller/TransactionController.java
@@ -5,19 +5,25 @@ import com.saurabh.frauddetection.service.ITransactionService;
 import com.saurabh.frauddetection.dto.TransactionRequest;
 import com.saurabh.frauddetection.dto.TransactionResponse;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Transaction API",
+        description = "Fraud detection transaction endpoints")
 public class TransactionController {
 
     private final ITransactionService transactionService;
 
+    @Operation(
+            summary = "Create transaction",
+            description = "Evaluates transaction for fraud and returns decision"
+    )
     @PostMapping("/transactions")
     public ResponseEntity<TransactionResponse> create(@Valid @RequestBody TransactionRequest request)
     {
@@ -27,6 +33,10 @@ public class TransactionController {
                 .body(response);
     }
 
+    @Operation(
+            summary = "Get transaction",
+            description = "Returns valid transaction on the basis of the transactionId, provided"
+    )
     @GetMapping("/transactions/{transactionId}")
     public ResponseEntity<Transaction> get(@PathVariable String transactionId)
     {
--- src/main/java/com/saurabh/frauddetection/dto/TransactionRequest.java
+++ src/main/java/com/saurabh/frauddetection/dto/TransactionRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 
 @NoArgsConstructor
@@ -13,13 +13,21 @@ import java.math.BigDecimal;
 @Setter
 @Builder
 public class TransactionRequest {
+
+    @Schema(example = "123")
     @NotNull(message = "userId is required")
     private Long userId;
+
+    @Schema(example = "75000")
     @NotNull(message = "amount is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "amount must be > 0")
     private BigDecimal amount;
+
+    @Schema(example = "IN")
     @NotBlank(message = "country is required")
     private String country;
+
+    @Schema(example = "AMAZON")
     @NotBlank(message = "merchantId is required")
     private String merchantId;
 }
